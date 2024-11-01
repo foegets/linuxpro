9c3dd151dd8d9b394acf987ec4c60dc956c31768
su <15975968351@163.com>
2024-11-01 08:39:25 -0400

    hp-lian

--- view.c
+++ view.c
@@ -5,13 +5,12 @@
 #include<stdlib.h>
 #include<dirent.h>
 #include<sys/types.h>
-#define PATH_MAX 20
+#define PATH_MAX 255
 #define CMD_MAX 30
 const int cnum=11; //number of cmd
 char cwd[PATH_MAX];
-char* cmd_argv[CMD_MAX];
-char allcmd[cnum][CMD_MAX]={"pwd2","cd2","ls2","touch2","echo2","cat2",
-"cp2","rm2","rename2","history2","quit"};
+char cmd_argv[CMD_MAX][CMD_MAX];
+char allcmd[11][CMD_MAX]={"pwd2","cd2","ls2","touch2","echo2","cat2","cp2","rm2","rename2","history2","quit"};
 void mygetcwd()
 {
     if(getcwd(cwd,sizeof(cwd))==NULL)
@@ -22,6 +21,7 @@ void mygetcwd()
 void pwd2()
 {
     //char cwd[PATH_MAX];
+    //memset(cwd,0,sizeof(char)*PATH_MAX);
     if(getcwd(cwd,sizeof(cwd))!=NULL)
     {
         printf("current directory:[%s]\n",cwd);
@@ -30,11 +30,19 @@ void pwd2()
     }
 }
 
-void cd2(const char * new_path)
+void cd2(const char* new_path)
 {
-    if(new_path==NULL||strcmp(new_path,""))chdir("home");
-    else if(strcmp(new_path,"-"))chdir("..");
-    else if(chdir(new_path)==0);
+    
+    if(new_path==NULL||strcmp(new_path,"")==0){
+        chdir("/home");
+        printf("/home\n");
+    }
+    else if(strcmp(new_path,"-")==0)
+    {
+        chdir("..");
+        printf("..\n");
+    }
+    else if(chdir(new_path)==0)printf("%s\n",new_path);
     else {
         printf("cd: %s: No such file or directory\n",new_path);
     }
@@ -49,6 +57,11 @@ void ls2(const char* dirname)
     {
         perror("ls: NO such file or directory\n");
         return;
+    }else{
+        while((entry=readdir(dirp))!=NULL)
+        {
+            
+        }
     }
 }
 void touch(){}
@@ -66,17 +79,26 @@ int main(int argc,char** argv)
     char cmd[CMD_MAX];
     printf("####### Welcome to shelldemo #########\n");
     while(1){
+    fflush(stdout);
+    mygetcwd();
     printf("[%s]>>",cwd);
+    //char ch;
+    //rsize_t strmax = sizeof cmd;
     memset(cmd,'\0',CMD_MAX);
+    //while((ch=getchar()) != '\n'&& ch != EOF);
     fgets(cmd,sizeof(cmd),stdin);
-    cmd_argv[0] = strtok(cmd," ");
-    int index=1;
-    while((cmd_argv[index++]=strtok(NULL," "))!=NULL);
+    cmd[strlen(cmd)-1]='\0';
+    char* ptr=cmd,*tmp=cmd;
+    //cmd_argv[0] = strtok_r(cmd," ",&ptr);
+    int index=0;
+    //cmd_argv[index]=cmd;
+    while(index<CMD_MAX&&(tmp=strtok_r(tmp," ",&ptr))!=NULL){strcpy(cmd_argv[index++],tmp);tmp=NULL;}
     int ci=0;
     while(ci<cnum&&strcmp(allcmd[ci],cmd_argv[0])!=0)ci++;
+    for(int i=0;i<index;++i)printf("%s ",cmd_argv[i]);
     switch(ci){
         case 0:pwd2();break;
-        case 1:cd2();break;
+        case 1:cd2(index>0?cmd_argv[1]:NULL);break;
         case 2:break;
         case 3:break;
         case 4:break;
@@ -88,8 +110,8 @@ int main(int argc,char** argv)
         case 10:break;
         case 11:break;
     }
-    fflush(stdout);
-    }|
+    
+    }
     printf("####### Quiting shelldemo #########\n");
     return 0;
 }
