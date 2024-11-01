eb6d7a7fdc7e2b77a948d6ecd2a2c81c6761ad1f
szs <15975968351@163.com>
2024-11-01 11:15:32 +0800

    lian->hp

--- view.c
+++ view.c
@@ -3,13 +3,21 @@
 #include<limits.h>
 #include<string.h>
 #include<stdlib.h>
+#include<dirent.h>
+#include<sys/types.h>
 #define PATH_MAX 20
 #define CMD_MAX 30
+const int cnum=11; //number of cmd
 char cwd[PATH_MAX];
-void view()
+char* cmd_argv[CMD_MAX];
+char allcmd[cnum][CMD_MAX]={"pwd2","cd2","ls2","touch2","echo2","cat2",
+"cp2","rm2","rename2","history2","quit"};
+void mygetcwd()
 {
-    printf("####### Welcome to shelldemo #########\n");
-
+    if(getcwd(cwd,sizeof(cwd))==NULL)
+    {
+        perror("getcwd error\n");
+    }
 }
 void pwd2()
 {
@@ -21,8 +29,28 @@ void pwd2()
         perror("getcwd error\n");
     }
 }
-void cd2(){}
-void ls2(){}
+
+void cd2(const char * new_path)
+{
+    if(new_path==NULL||strcmp(new_path,""))chdir("home");
+    else if(strcmp(new_path,"-"))chdir("..");
+    else if(chdir(new_path)==0);
+    else {
+        printf("cd: %s: No such file or directory\n",new_path);
+    }
+    mygetcwd();
+}
+
+void ls2(const char* dirname)
+{
+    DIR *dirp;
+    struct dirent* entry;
+    if((dirp=opendir(dirname))==NULL)
+    {
+        perror("ls: NO such file or directory\n");
+        return;
+    }
+}
 void touch(){}
 void echo2(){}
 void cat2(){}
@@ -31,11 +59,37 @@ void rm2(){}
 void rename2(){}
 void history2(){}
 void quit(){}
+void err(){}
+
 int main(int argc,char** argv)
 {
     char cmd[CMD_MAX];
     printf("####### Welcome to shelldemo #########\n");
+    while(1){
     printf("[%s]>>",cwd);
+    memset(cmd,'\0',CMD_MAX);
+    fgets(cmd,sizeof(cmd),stdin);
+    cmd_argv[0] = strtok(cmd," ");
+    int index=1;
+    while((cmd_argv[index++]=strtok(NULL," "))!=NULL);
+    int ci=0;
+    while(ci<cnum&&strcmp(allcmd[ci],cmd_argv[0])!=0)ci++;
+    switch(ci){
+        case 0:pwd2();break;
+        case 1:cd2();break;
+        case 2:break;
+        case 3:break;
+        case 4:break;
+        case 5:break;
+        case 6:break;
+        case 7:break;
+        case 8:break;
+        case 9:break;
+        case 10:break;
+        case 11:break;
+    }
     fflush(stdout);
+    }|
+    printf("####### Quiting shelldemo #########\n");
     return 0;
 }
